977c0be2f33821dddccbcf2691d3de1ac2f9e4cb
Radha Hariharan <radhahariharan296@gmail.com>
2026-06-23 21:26:06 +0530

    Refactor code structure for improved readability and maintainability

---
+++ examples/nextjs/src/app/page.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import { createFormStore, createStringValidator, toValidationRule } from "@headless-form/core";
+
+interface myForm {
+  firstname: string,
+  lastname: string,
+  email: string,
+}
+
+export default function Home() {
+  const form = createFormStore<myForm>({
+    mode: "uncontrolled",
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+    },
+    validate: {
+      firstname: toValidationRule(createStringValidator({
+        required: true,
+        minLength: 4,
+        maxLength: 12
+      })),
+      lastname: toValidationRule(createStringValidator({
+        required: true,
+        minLength: 4,
+        maxLength: 12
+      })),
+      email: toValidationRule(createStringValidator({
+        required: true,
+      }))
+    }
+  })
+
+  const submit = () => {
+    form.validate();
+    console.log(form.getValues())
+    console.log(form.errors)
+  }
+
+  return (
+    <div>
+      <input type="text" key={form.key("email")} {...form.getInputProps("email")} />
+      <br />
+      <input type="text" key={form.key("firstname")} {...form.getInputProps("firstname")} />
+      <br />
+      <input type="text" key={form.key("lastname")} {...form.getInputProps("lastname")} />
+
+      <br />
+      Email - {form.getValues().email}
+      <br />
+      FirstName - {form.getValues().firstname}
+      <br />
+      LastName - {form.getValues().lastname}
+      <br />
+      <button onClick={submit}>Submit</button>
+    </div>
+  );
+}
